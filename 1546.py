3f59250caec9a3d6450b6654eba24e447e3aa3b0
hhali333 <dimsum4321@naver,com>
2022-06-26 18:02:47 +0900

    message

---
+++ 1546.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+avg = 0
+maxscore = 0
+
+
+aa = list(map(int, input().split()))
+maxscore = max(aa)
+
+
+for i in range(n) :
+    aa[i] = (aa[i] / maxscore) * 100
+    avg += aa[i]
+
+avg = avg / n
+print(format(avg, ".2f"))
