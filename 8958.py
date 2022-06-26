06a1083880380885f39b9ff562c9c9712e72fbb1
hhali333 <dimsum4321@naver,com>
2022-06-26 17:47:54 +0900

    8958번

---
+++ 8958.py
@@ -0,0 +1,16 @@
+t = int(input())
+
+for i in range(t) :
+    b = input()
+    s = list(b)
+    sum = 0
+    c = 1
+
+    for i in s :
+        if i == 'O' :
+            sum += c
+            c += 1
+        else :
+            c = 1
+
+    print(sum)
